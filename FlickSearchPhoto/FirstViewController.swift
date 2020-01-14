be9ec009a8b1eb8578c479b86f855d1bc38a32ae
onion <onion.zhang@langlive.com>
2020-01-14 21:30:30 +0800

    Initial Commit

---
+++ FlickSearchPhoto/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FlickSearchPhoto
+//
+//  Created by 張聰益 on 2020/1/14.
+//  Copyright © 2020 張聰益. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FlickSearchPhoto/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FlickSearchPhoto
+//
+//  Created by 張聰益 on 2020/1/14.
+//  Copyright © 2020 張聰益. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
